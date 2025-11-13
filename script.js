4d305beeba949eca9c3512c598bb3f26ddded458
GermanDCruzG <german_cruz@hotmail.com>
2025-11-13 16:55:31 -0500

    Agregué archivos de Portafolio

---
+++ script.js
@@ -0,0 +1,8 @@
+//Ejercício de código Javascript de Generation
+//Creado por: Germán D Cruz G.
+//Portafolio
+
+// Esta función se usa cuando el usuario hace clic en "Ver más"
+function mostrarMensaje(nombreProyecto) {
+    alert("Abriste " + nombreProyecto);
+}
