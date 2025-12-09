ac96f07d3818e00ed6e643bac1be222c6f8132c3
GermanDCruzG <german_cruz@hotmail.com>
2025-12-09 16:59:21 -0500

    Agregué archivos de Portafolio.

---
+++ script.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById("btnContacto");
+btn.addEventListener("click", function () {
+    alert("Puedes contactarme al correo: germandcruzg@gmail.com");
+});
+
+let btnMenu = document.getElementById("btnMenu");
+let menu = document.getElementById("menu");
+
+btnMenu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
